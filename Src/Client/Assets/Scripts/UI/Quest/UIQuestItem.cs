7478151a8989629230b44e1a2cc8ed26491b264b
ysweets <1418103558@qq.com>
2023-07-02 10:02:47 +0800

    TaskSystem

---
+++ Src/Client/Assets/Scripts/UI/Quest/UIQuestItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Common.Data;
+using Managers;
+using Models;
+using SkillBridge.Message;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UIQuestItem : ListView.ListViewItem
+{
+    public Text title;
+    public Image background;
+    public Sprite normalBg;
+    public Sprite selectedBg;
+
+    public override void onSelected(bool selected)
+    {
+        this.background.overrideSprite = selected ? selectedBg : normalBg;
+    }
+
+    public Quest quest;
+    void Start(){
+
+    }
+
+    public void SetQuestInfo(Quest item)
+    {
+        this.quest = item;
+        if (this.title != null) this.title.text = this.quest.Define.Name;
+    }
+}
---
+++ Src/Client/Assets/Scripts/UI/Quest/UIQuestSystem.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Common.Data;
+using Managers;
+using Models;
+using SkillBridge.Message;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIQuestSystem : UIWindow {
+    public Text title;
+
+    public GameObject itemPrefab;
+
+    public TabView Tabs;
+    public ListView listMain;
+    public ListView listBranch;
+
+    public UIQuestInfo questInfo;
+
+    private bool showAvailableList = false;
+
+    void Start()
+    {
+        this.listMain.onItemSelected += this.OnQuestSelected;
+        this.listBranch.onItemSelected += this.OnQuestSelected;
+        this.Tabs.OnTabSelect += OnSelectTab;
+        RefreshUI();
+        //QuestManager.Instance.OnQuestChanged += RefreshUI;
+    }
+
+    void OnSelectTab(int idx)
+    {
+        showAvailableList = idx == 1;
+        RefreshUI();
+    }
+    private void OnDestroy()
+    {
+        //QuestManager.Instance.OnQuestChanged -= RefreshUI;
+    }
+
+    void RefreshUI()
+    {
+        ClearAllQuestList();
+        InitAllQuestItems();
+    }
+
+    void InitAllQuestItems()
+    {
+        foreach(var kv in QuestManager.Instance.allQuests)
+        {
+            if (showAvailableList)
+            {
+                if (kv.Value.Info != null)
+                    continue;
+            }
+            else
+            {
+                if (kv.Value.Info == null)
+                    continue;
+                else if (kv.Value.Info.Status == QuestStatus.Finished)
+                    continue;
+            }
+
+            GameObject go = Instantiate(itemPrefab, kv.Value.Define.Type == QuestType.Main ? this.listMain.transform : this.listBranch.transform);
+            UIQuestItem ui = go.GetComponent<UIQuestItem>();
+            ui.SetQuestInfo(kv.Value);
+            
+            this.listMain.AddItem(ui);
+            this.listBranch.AddItem(ui);
+        }
+    }
+
+    void ClearAllQuestList()
+    {
+        this.listMain.RemoveAll();
+        this.listBranch.RemoveAll();
+    }
+
+    public void OnQuestSelected(ListView.ListViewItem item)
+    {
+        UIQuestItem questItem = item as UIQuestItem;
+        this.questInfo.SetQuestInfo(questItem.quest);
+    }
+}
